d37d013132662b827152b631c9df51193c1f94a7
Henriques Maculungungu <henriqueswaldemar1@gmail.com>
2026-06-12 14:48:21 +0000

    featy: auth middleware added

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import type { Request, Response, NextFunction } from "express";
+import { env } from "../config/env";
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, env.JWT_SECRET);
+    (req as any).user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
