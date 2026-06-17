26368acb93231472b42b1b061bce7796eb852a13
Henriques Maculungungu <henriqueswaldemar1@gmail.com>
2026-06-17 14:28:31 +0000

    feat: auth miuddleware added into connections and partners routes

--- src/routes/connection.routes.ts
+++ src/routes/connection.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { connectionController } from "../controllers/connection.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/", connectionController.send);
+router.post("/", authMiddleware,connectionController.send);
 router.get("/my", connectionController.my);
 
 router.patch("/:id/accept", connectionController.accept);
--- src/routes/partner.routes.ts
+++ src/routes/partner.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { partnerController } from "../controllers/partner.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/", partnerController.create);
+router.post("/", authMiddleware,partnerController.create);
 router.get("/", partnerController.findAll);
 router.get("/:id", partnerController.findById);
 router.patch("/:id", partnerController.update);
 
 router.get("/:id/benefits", partnerController.getBenefits);
-router.post("/:id/benefits", partnerController.createBenefit);
+router.post("/:id/benefits", authMiddleware, partnerController.createBenefit);
 
 export default router;
