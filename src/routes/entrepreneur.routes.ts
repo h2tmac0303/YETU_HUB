46806e3dd6b886b690a5120792a44dbfc96fb367
Henriques Maculungungu <henriqueswaldemar1@gmail.com>
2026-06-17 13:45:35 +0000

    feat: Authmiddleware added to Investors and entrepreneur routes

--- src/routes/entrepreneur.routes.ts
+++ src/routes/entrepreneur.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { entrepreneurController } from "../controllers/entrepreneur.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/", entrepreneurController.create);
+router.post("/", authMiddleware,entrepreneurController.create);
 router.get("/", entrepreneurController.findAll);
 router.get("/:id", entrepreneurController.findById);
 router.patch("/:id", entrepreneurController.update);
--- src/routes/investor.routes.ts
+++ src/routes/investor.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { investorController } from "../controllers/investor.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/", investorController.create);
+router.post("/",authMiddleware, investorController.create);
 router.get("/", investorController.findAll);
 router.get("/:id", investorController.findById);
 router.patch("/:id", investorController.update);
